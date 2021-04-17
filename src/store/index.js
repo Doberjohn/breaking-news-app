66e68066363cb170880eb70dc665c7f266a11225
John Fanidis <johnfanidis@gmail.com>
2021-04-17 20:53:07 +0300

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "../plugins/axios";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+   state: {
+      countries: [{
+         name: "United States of America",
+         value: "us",
+      },
+         {
+            name: "Nigeria",
+            value: "ng",
+         },
+         {
+            name: "Argentina",
+            value: "ar",
+         },
+         {
+            name: "Canada",
+            value: "ca",
+         },
+         {
+            name: "South Africa",
+            value: "za",
+         },
+      ],
+      categories: [
+         "entertainment",
+         "general",
+         "health",
+         "science",
+         "business",
+         "sports",
+         "technology",
+      ],
+   },
+   mutations: {},
+   actions: {
+      async getTopNews(context, country) {
+         return axios({
+            url: `/top-headlines?country=${country}`,
+            method: "GET",
+         });
+      },
+   },
+});
+export default store;
